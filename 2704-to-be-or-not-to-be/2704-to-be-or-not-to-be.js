e8da29816e0c1084e56e84e1426681c0a0179008
Simran Nadaf <simrannadaf2608@gmail.com>
2026-09-25 22:12:21 +0530

    Time: 45 ms (47.85%), Space: 54 MB (22.92%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe(val1){
+            if (val === val1) return true
+            else throw Error("Not Equal")
+        },
+        notToBe(val1){
+            if (val !== val1 ) return true
+            else throw Error("Equal")
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
